3ccb424240684d9644ca1fb309893437bc51c518
jmk-001 <junmin.kim.01@gmail.com>
2025-05-05 11:14:41 +0800

    Implement skeleton data structure for ukkonen

--- ukkonen.py
+++ ukkonen.py
@@ -11,3 +11,22 @@ class Edge:
         self.start = start
         self.end = end
         self.target = target
+
+class Ukkonen:
+    def __init__(self, text:str) -> None:
+        self.text = text
+        self.suffix_tree = self.generate_suffix_tree(text)
+
+    def generate_suffix_tree(self, text:str) -> Node:
+        n = len(text)
+
+        global_end = [0]
+        root = Node()
+        active_node = root
+        active_edge = None
+        active_len = 0
+        prev_internal_node = None
+
+        # Perform tree generation
+    
+        return root
